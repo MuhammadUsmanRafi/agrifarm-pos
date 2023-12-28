0a8bd010c0abf75e237c45b773567562fc1f60db
MuhammadUsmanRafi <102518937+MuhammadUsmanRafi@users.noreply.github.com>
2023-12-28 19:07:57 +0500

    update search filter

--- ViewInventoryModulesInterface.py
+++ ViewInventoryModulesInterface.py
@@ -49,8 +49,63 @@ class ViewInventoryModulesInterface:
 
         self.outer_frame.bind("<Configure>", lambda event, canvas=self.canvas: self.on_frame_configure(canvas))
 
-        products_data = product.find()
+        # Search frame
+        self.search_frame = Frame(self.window, bg="white", highlightbackground="#968802", highlightthickness=0)
+        self.search_frame.place(x=self.window.winfo_screenwidth() / 2 - 100, y=self.window.winfo_screenheight() / 8)
 
+        # Search label
+        self.search_label = Label(self.search_frame, text="Search: ", font=("Arial", 12, "bold"),
+                                  background="white")
+        self.search_label.pack(side=LEFT, padx=(0, 10))
+
+        # User entry for search
+        self.user_entry = Entry(self.search_frame, font=("Arial", 12), bg="white", validate="key")
+        self.user_entry.focus_set()
+        self.user_entry.pack(fill="x", expand=True)
+
+        # Search button
+        self.search_button = Button(self.search_frame, text="Search", command=lambda: self.perform_search(num),
+                                    font=("Arial", 12), background="#968802", foreground="white")
+        self.search_button.pack(side=LEFT, padx=(10, 0))
+
+        # Display all products initially
+        self.display_products(num)
+
+    def on_frame_configure(self, canvas):
+        canvas.configure(scrollregion=canvas.bbox("all"))
+
+    def menu_interface(self):
+        InventoryManagementInterface.InventoryManagementInterface(self.window)
+
+    def button_click(self, product_name, company, brand, category, image, count, rate, num):
+        if num == 2 or num == 0:
+            UpdateInventoryModuleInterface.UpdateInventoryModuleInterface(self.window, product_name, company, brand,
+                                                                          category, image, count, rate)
+        if num == 3:
+            user_response = messagebox.askyesno("Confirm Remove", f"Do you want to remove {product_name}?")
+            if user_response:
+                product.delete_one({"ProductName": product_name, "ProductCompany": company, "ProductBrand": brand})
+                self.display_products(num)
+
+    def perform_search(self, num):
+        search_term = self.user_entry.get().lower()
+        if search_term:
+            # Filter products based on the search term
+            filtered_products = [product_data for product_data in product.find()
+                                 if search_term in product_data["ProductName"].lower()]
+        else:
+            # If search term is empty, display all products
+            filtered_products = product.find()
+
+        # Update the displayed products
+        self.update_display(filtered_products, num)
+
+    def update_display(self, products_data, num):
+        # Clear existing frames
+        for widget in self.outer_frame.winfo_children():
+            widget.destroy()
+
+        # Display the filtered products
         for i, product_data in enumerate(products_data):
             frame = Frame(self.outer_frame, bg="#487307", highlightbackground="#487307", highlightthickness=0)
             frame.grid(row=0, column=i, padx=5, pady=5)
@@ -83,9 +138,9 @@ class ViewInventoryModulesInterface:
             button_text = "View" if num != 2 else "Update" if num == 2 else "Remove"
             button = Button(frame, text=button_text, font=("Arial", 12),
                             command=lambda name=product_name, comp=company, pbrand=brand, pcategory=category,
-                                           path=image_data, c=count, r=rate: self.button_click(name, comp, pbrand,
-                                                                                               pcategory, path, c, r,
-                                                                                               num), width=20,
+                                           path=image_data, c=count, r=rate, n=num: self.button_click(name, comp, pbrand,
+                                                                                                 pcategory, path, c, r,
+                                                                                                 n), width=20,
                             background="#968802", foreground="white")
             button.pack(side=TOP, padx=10, pady=5)
 
@@ -94,35 +149,10 @@ class ViewInventoryModulesInterface:
             elif num == 3:
                 button.config(text="Remove")
 
-            # Search frame
-            self.search_frame = Frame(self.window, bg="white", highlightbackground="#968802", highlightthickness=0)
-            self.search_frame.place(x=self.window.winfo_screenwidth() / 2 - 100, y=self.window.winfo_screenheight() / 8)
-
-            # Search label
-            self.search_label = Label(self.search_frame, text="Search: ", font=("Arial", 12, "bold"),
-                                      background="white")
-            self.search_label.pack(side=LEFT, padx=(0, 10))
-
-            # User entry for search
-            self.user_entry = Entry(self.search_frame, font=("Arial", 12), bg="white")
-            self.user_entry.focus_set()
-            self.user_entry.pack(fill="x", expand=True)
-
-    def on_frame_configure(self, canvas):
-        canvas.configure(scrollregion=canvas.bbox("all"))
-
-    def menu_interface(self):
-        InventoryManagementInterface.InventoryManagementInterface(self.window)
-
-    def button_click(self, product_name, company, brand, category, image, count, rate, num):
-        if num == 2 or num == 0:
-            UpdateInventoryModuleInterface.UpdateInventoryModuleInterface(self.window, product_name, company, brand,
-                                                                          category, image, count, rate)
-        if num == 3:
-            user_response = messagebox.askyesno("Confirm Remove", f"Do you want to remove {product_name}?")
-            if user_response:
-                product.delete_one({"ProductName": product_name, "ProductCompany": company, "ProductBrand": brand})
-                ViewInventoryModulesInterface(self.window, 3)
+    def display_products(self, num):
+        # Display all products initially
+        products_data = product.find()
+        self.update_display(products_data, num)
 
 
 if __name__ == "__main__":
